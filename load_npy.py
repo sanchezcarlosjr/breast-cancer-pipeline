98a18db60879299744bda652cd22b9192b2670a8
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <24639141+sanchezcarlosjr@users.noreply.github.com>
2023-04-20 16:18:36 -0700

    Update load_npy.py

--- load_npy.py
+++ load_npy.py
@@ -3,8 +3,9 @@ import io
 import cv2
 import numpy as np
 import sys
+from decouple import config 
 
-response = requests.get(sys.argv[1])
+response = requests.get(config('REST')+sys.argv[1])
 response.raise_for_status()
 image = np.load(io.BytesIO(response.content))
 cv2.imshow('NPY Image',image)
